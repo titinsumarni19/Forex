6c4eadad20f24902751f09e2662a9d4ccf17aeab
titinsumarni19 <92568394+titinsumarni19@users.noreply.github.com>
2023-06-14 09:48:54 +0700

    Initial commit

---
+++ app/src/main/java/com/titinsumarni202102246/forex/RootModel.java
@@ -0,0 +1,19 @@
+package com.titinsumarni202102246.forex;
+
+import com.google.gson.annotations.SerializedName;
+
+public class RootModel {
+
+    @SerializedName("rates")
+    private RatesModel ratesModel;
+
+    public  RootModel() {}
+
+    public RatesModel getRatesModel() {
+        return ratesModel;
+    }
+
+    public void setRatesModel(RatesModel ratesModel) {
+        this.ratesModel = ratesModel;
+    }
+}
